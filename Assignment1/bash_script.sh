a17fcc6d953ae73beebef7bda5211b9b8386d4ec
AakankshaBhat <97674377+AakankshaBhat@users.noreply.github.com>
2022-01-24 12:12:51 +0530

    Create bash_script.sh

---
+++ Assignment1/bash_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#make directory and sub-directories
+mkdir -p /tmp/output/{LEED_projects_VA,Num_LEED_projects,Gross_sqfeet_space,Num_Project_Zip_code,Diff_CAnVI}
+#run python file
+C:/Users/91738/Practice_Python/Assign_1_pyspark.ipynb
